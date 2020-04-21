10713e50a5b0256dfec4bd13dd49541d49b76937
Jibin Reji <jibinsamreji@github.com>
2020-04-21 02:40:28 -0400

    Initial commit

---
+++ first.py
@@ -0,0 +1,8 @@
+patient_name = 'John Smith'
+patient_age = 20
+is_newP = True
+
+print('Patient Name:', patient_name,'\n', 'Age:',patient_age, '\n', 'New Patient:',is_newP)
+
+patient_weight = input('What is your weight in Kg? ')
+print(patient_name+'  weighs at '+patient_weight)
