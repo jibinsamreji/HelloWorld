b790d77a74f293abec1d15eceb13a121a32bd81e
Jibin Reji <jibinsamreji@github.com>
2020-04-30 09:15:44 -0400

    Data Lists!

--- functions.py
+++ functions.py
@@ -1,8 +1,9 @@
 # def-defining a function, leave two spaces down after definition
+# return-used to return data from a defined function
 
 
 def greet_user(first_name, last_name):
-    print(f"Hi {first_name}  {last_name}!")
+    print(f"Hi {first_name} {last_name}!")
     print("Welcome aboard")
 
 
@@ -10,5 +11,14 @@ def greet_user(first_name, last_name):
 print("Start")
 greet_user("Jibin", "Reji")
 greet_user("Emily", "Reji")
+
 greet_user(last_name="Reji", first_name="Jeffin")
+
 print("Finish")
+
+
+def square(number):
+    return number * number
+
+
+print(square(3))
