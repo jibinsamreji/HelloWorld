3288329cbdec8488d4829c04bd57c76ed90dc0a5
Jibin Reji <jibinsamreji@github.com>
2020-05-03 11:37:04 -0400

    Calculator

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from ecommerce.shipping import calc_shipping
+
 calc_shipping()
 calc_shipping()
+
 """import ecommerce.shipping
 ecommerce.shipping.calc_shipping()"""
+
+# from ecommerce import shipping
---
+++ calculator.py
@@ -0,0 +1,54 @@
+print(" WELCOME! \n CAL BUDDY IS READY TO ASSIST YOU WITH YOUR CALCULATIONS :)\n \n")
+
+print(" What operation would you like to do now: \n Please choose from the following\n"
+      "1 --> Addition\n2 --> Subtraction\n3 --> Multiplication\n4 --> Division\n")
+
+
+def ops():
+    choice = input("Your choice:")
+    if choice == "1":
+        print("-------Addition--------\n")
+        x = int(input("Enter first number:"))
+        y = int(input("Enter second number:"))
+        res = x + y
+        print("Result: " + str(res))
+
+    elif choice == "2":
+        print("-------Subtraction--------\n")
+        x = int(input("Enter first number:"))
+        y = int(input("Enter second number:"))
+        res = x - y
+        print("Result: " + str(res))
+
+    elif choice == "3":
+        print("-------Multiplication--------\n")
+        x = int(input("Enter first number:"))
+        y = int(input("Enter second number:"))
+        res = x * y
+        print("Result: " + str(res))
+
+    elif choice == "4":
+        print("-------Division--------\n")
+        x = int(input("Enter first number:"))
+        y = int(input("Enter second number:"))
+        res = x / y
+        print("Result: " + str(res))
+
+    else:
+        print("Invalid Entry! Please try again: ")
+        ops()
+
+
+ops()
+
+cont_op = input("\nAnymore further calculation? Y/N: ").capitalize()
+
+if cont_op == 'Y' or cont_op == 'YES':
+    {
+        ops()
+    }
+else:
+    {
+        print("\n \nThank You For Using CAL BUDDY! :)")
+    }
+
